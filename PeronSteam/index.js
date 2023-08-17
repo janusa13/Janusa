3b59b7c03eadb545e00340172d933d3cb0a65749
Janusa13 <facundojanusa@gmail.com>
2023-08-17 19:01:53 -0300

    actualizacion de front

--- PeronSteam/index.js
+++ PeronSteam/index.js
@@ -1,44 +1,8 @@
-import express, { response } from "express";
-import cors from "cors";
-import morgan from "morgan";
-import games from "./games.json" assert { type: "json" };
-const app = express();
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.get("/game/:name", async (rec, res) => {
-  const resultGameId = findGame(rec.params.name);
-  if (resultGameId) {
-    const infoGame = await consulta(resultGameId.appid);
-    res.json({ mensaje: infoGame });
-  } else {
-    res.status(404).json({ mensaje: "Juego no encontrado" });
-  }
-});
-app.listen(3000, () => {
-  console.log("server running");
-});
-
-function findGame(name) {
-  const gameName = games.applist.apps.find((game) => game.name === name);
-  return gameName;
-}
-
-async function consulta(idGame) {
-  return fetch(
-    "https://store.steampowered.com/api/appdetails?appids=" + idGame
-  ).then((response) => response.json());
-}
-
-async function buscarJuego() {
-  const gameName = document.getElementById("gameName").value;
-
-  if (gameName) {
-    const response = await fetch(`/game/${encodeURIComponent(gameName)}`);
-    const data = await response.json();
-
-    const resultadoDiv = document.querySelector(".resultado");
-    resultadoDiv.innerHTML = JSON.stringify(data, null, 2);
-  }
+const name = document.getElementById(gameName).value;
+function buscarJuego() {
+  fetch("http://localhost:3000/game${}")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
---
+++ PeronSteam/servAPI.js
@@ -0,0 +1,32 @@
+import express, { response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import games from "./games.json" assert { type: "json" };
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.get("/game/:name", async (rec, res) => {
+  const resultGameId = findGame(rec.params.name);
+  if (resultGameId) {
+    const infoGame = await consulta(resultGameId.appid);
+    res.json(infoGame);
+  } else {
+    res.status(404).json({ mensaje: "Juego no encontrado" });
+  }
+});
+app.listen(3000, () => {
+  console.log("server running");
+});
+
+function findGame(name) {
+  const gameName = games.applist.apps.find((game) => game.name === name);
+  return gameName;
+}
+
+async function consulta(idGame) {
+  return fetch(
+    "https://store.steampowered.com/api/appdetails?appids=" + idGame
+  ).then((response) => response.json());
+}
